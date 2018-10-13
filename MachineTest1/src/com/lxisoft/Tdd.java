88524db2f87d0056490bd203fee9ed1d495e1198
sanoop sanu <sanoop.p@lxisoft.com>
2018-10-13 21:49:39 +0530

    calculator sample code

--- MachineTest1/src/com/lxisoft/Tdd.java
+++ MachineTest1/src/com/lxisoft/Tdd.java
@@ -3,20 +3,33 @@ import java.util.Scanner;
 public class Tdd {
 
 	public static void main(String[] args) {
-		int num;
+		int add,mul,sub,div,mod,num1,num2,res;
 		Scanner s= new Scanner(System.in);
-		System.out.println("Enter any number just see even or odd");
-		num=s.nextInt();
-		if(num%2==0){
-			
-			System.out.println(num+"even number");
+		System.out.println("Enter First Number");
+		num1=s.nextInt();
+		System.out.println("Enter Second Number");
+		num2=s.nextInt();
+		add=num1+num2;
+		mul=num1*num2;
+		sub=num1-num2;
+		div=num1/num2;
+		mod=num1%num2;
+		
+		System.out.println("                     Select ur choice\n                     ----------------  \n1.add   2.multiplication   3.substraction   4.division   5.mode");
+		
+		res=s.nextInt();
+		switch(res){
+		
+		case 1:System.out.println(add);
+		break;
+		case 2:System.out.println(mul);
+		break;
+		case 3:System.out.println(sub);
+		break;
+		case 4:System.out.println(div);
+		break;
+		case 5:System.out.println(mod);
 		}
-		else 
-		{
 			
-			System.out.println(num+"odd number");
-		}
-	
-	}
-
+}
 }
