4f80d607b6025c5001adf5c656b99909fe6bed53
sanoop sanu <sanoop.p@lxisoft.com>
2018-10-16 20:10:49 +0530

    decimal to binary connection

---
+++ Interview/src/com/lxisoft/Tdd.java
@@ -0,0 +1,14 @@
+package com.lxisoft;
+
+public class Tdd {
+
+	public static void main(String[] args) {
+		System.out.println(Integer.toBinaryString(10));
+		System.out.println(Integer.toBinaryString(20));
+		System.out.println(Integer.toBinaryString(5000));
+
+		
+		
+	}
+
+}
